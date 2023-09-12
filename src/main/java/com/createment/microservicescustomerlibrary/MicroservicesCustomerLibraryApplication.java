7a5271e501983eb88a1caa06ba1330f7d2c2cc6b
Sam Boosten <boostensam@gmail.com>
2023-09-12 10:20:32 +0200

    First commit

---
+++ src/main/java/com/createment/microservicescustomerlibrary/MicroservicesCustomerLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.createment.microservicescustomerlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesCustomerLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesCustomerLibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/createment/microservicescustomerlibrary/MicroservicesCustomerLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.createment.microservicescustomerlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesCustomerLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
